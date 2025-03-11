0c363b142f82fa74870286256dae7f13e0f3b78a
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2025-03-11 16:38:21 +0100

    documentation adjustment

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,7 +18,13 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='api_docs'), #documentation
+    # API Doc Link
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     
     # App urls
     path('api/user/', include('user.urls')),
