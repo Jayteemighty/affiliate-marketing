8e8840882252b4bf2b923d8b8c65e28d1474e9cb
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2025-03-15 00:05:38 +0100

    use vercel for hosting backend

--- frontend/src/libs/constants.ts
+++ frontend/src/libs/constants.ts
@@ -1,3 +1,5 @@
 export const BASE_URL = "https://backend-api.theprofitplus.com.ng";
 // export const BASE_URL2 = "http://127.0.0.1:8000";
-export const BASE_URL2 = "https://pp-4trb.onrender.com";
+// export const BASE_URL2 = "https://pp-4trb.onrender.com";
+export const BASE_URL2 = "https://pp-beige.vercel.app";
+
